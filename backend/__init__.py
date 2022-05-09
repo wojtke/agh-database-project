cef4e5b73cfe53c8b29d1358b0678360436b0b7f
Wojciech Jasiński <woj.jasinski@gmail.com>
2022-05-09 18:44:25 +0200

    cors problems maybe fixed

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,15 +1,16 @@
 from pymongo.collection import Collection
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 import dotenv
 dotenv.load_dotenv()
 
 app = Flask(__name__)
 app.config.from_prefixed_env()
+CORS(app)
 
 pymongo = PyMongo(app)
-
 recipes: Collection = pymongo.db.recipes
 
 
